6a7c7af94f9249bb97f1d4351cdaa4295d260eb0
SerdarHoja <serdarhoja@gmail.com>
2023-11-29 00:28:39 +0500

    object v1

--- src/services/objects.service.js
+++ src/services/objects.service.js
@@ -4,7 +4,9 @@ import authHeader from './auth-header';
 const API_URL = 'http://crm.web-hands.ru/api/v1';
 
 class ObjectsService {
-  
+    getObjects(section) {
+        return axios.get(API_URL + '/objects/list/?section=' + section, { headers: authHeader() });
+    }
 }
 
 export default new ObjectsService();
--- src/views/objects/index.vue
+++ src/views/objects/index.vue
@@ -11,6 +11,70 @@
                 </a-button> -->
             </template>
         </a-page-header>
-        <!-- {{ objects }} -->
+        <div class="country-data">
+            <div class="mt-5 p-12">
+                <div class="flex gap-5">
+                    <div class="w-1/5">
+                        <h4 class="text-[#A5A7A7]">Объект</h4>
+                    </div>
+                    <div class="w-1/6">
+                        <h4 class="text-[#A5A7A7]">Информация</h4>
+                    </div>
+                    <div class="w-1/6">
+                        <h4 class="text-[#A5A7A7]">Характеристики</h4>
+                    </div>
+                    <div class="w-1/6">
+                        <h4 class="text-[#A5A7A7]">Контакты</h4>
+                    </div>
+                    <div class="w-1/6">
+                        <h4 class="text-[#A5A7A7]">Статус</h4>
+
+                    </div>
+                    <div class="w-1/6">
+                        <h4 class="text-[#A5A7A7]">Выгрузка</h4>
+
+                    </div>
+                </div>
+            </div>
+            <div v-for="obj in countryObjects" :key="obj.id">
+                <ObjectItem :object="obj" />
+            </div>
+        </div>
     </div>
 </template>
+<script setup>
+    import { ref, onMounted, computed } from 'vue'
+    import { useObjectsStore } from '@/stores/objects.module.js';
+    import { message } from 'ant-design-vue';
+    import { TrashIcon } from '@heroicons/vue/24/solid'
+    import ObjectItem from '@/components/objects/ObjectItem.vue';
+
+    const myStore = useObjectsStore();
+    const loading = ref(false);
+
+    onMounted(() => {
+        fetchCountryData();
+    })
+
+    const countryObjects = computed(() => {
+        return myStore.countryObjects;
+    })
+
+    const fetchCountryData = async () => {
+        loading.value = true;
+        try {
+            await myStore.getObjects('country').then(
+            (response) => {
+                if (response.data.result === 'error') {
+                    message.error(response.data.text)
+                    loading.value = false;
+                } else {
+                    loading.value = false;
+                }
+            }
+            )
+        } catch (error) {
+            console.error('Error fetching data in component:', error);
+        }
+    };
+</script>
