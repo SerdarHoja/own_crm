60b16fa3c78fa72f0b0f8aec52bbf3ff0aad9981
stanislavmuhin <stnslvmukhin@gmail.com>
2024-02-27 20:02:39 +0300

    print currency rate, delete search field

--- src/components/HeaderComponent.vue
+++ src/components/HeaderComponent.vue
@@ -3,28 +3,28 @@
     <a href="#" class="mr-auto">
       <img src="../assets/img/logo.png" alt="logo" class="w-full max-w-logoW">
     </a>
-    <a-input-search
-        v-model:value="searchText"
-        class="mr-12"
-        size="middle"
-        placeholder="Поиск"
-        style="max-width: 20rem"
-        @search="onSearch"
-    >
-      <template #enterButton>
-        <a-button class="px-3">
-          <svg width="18" height="18" viewBox="0 0 22 22" fill="none" xmlns="http://www.w3.org/2000/svg">
-            <path fill-rule="evenodd" clip-rule="evenodd" d="M19.0153 8.97223C19.0153 12.2706 16.3414 14.9445 13.0431 14.9445C9.74468 14.9445 7.07082 12.2706 7.07082 8.97223C7.07082 5.67386 9.74468 3 13.0431 3C16.3414 3 19.0153 5.67386 19.0153 8.97223ZM20.0153 8.97223C20.0153 12.8229 16.8937 15.9445 13.0431 15.9445C11.2977 15.9445 9.70222 15.3032 8.47924 14.2434L2.70613 20.0165L1.99902 19.3094L7.77211 13.5363C6.71219 12.3133 6.07082 10.7177 6.07082 8.97223C6.07082 5.12157 9.1924 2 13.0431 2C16.8937 2 20.0153 5.12157 20.0153 8.97223Z" fill="#4064BF"/>
-          </svg>
-        </a-button>
-      </template>
-    </a-input-search>
+<!--    <a-input-search-->
+<!--        v-model:value="searchText"-->
+<!--        class="mr-12"-->
+<!--        size="middle"-->
+<!--        placeholder="Поиск"-->
+<!--        style="max-width: 20rem"-->
+<!--        @search="onSearch"-->
+<!--    >-->
+<!--      <template #enterButton>-->
+<!--        <a-button class="px-3">-->
+<!--          <svg width="18" height="18" viewBox="0 0 22 22" fill="none" xmlns="http://www.w3.org/2000/svg">-->
+<!--            <path fill-rule="evenodd" clip-rule="evenodd" d="M19.0153 8.97223C19.0153 12.2706 16.3414 14.9445 13.0431 14.9445C9.74468 14.9445 7.07082 12.2706 7.07082 8.97223C7.07082 5.67386 9.74468 3 13.0431 3C16.3414 3 19.0153 5.67386 19.0153 8.97223ZM20.0153 8.97223C20.0153 12.8229 16.8937 15.9445 13.0431 15.9445C11.2977 15.9445 9.70222 15.3032 8.47924 14.2434L2.70613 20.0165L1.99902 19.3094L7.77211 13.5363C6.71219 12.3133 6.07082 10.7177 6.07082 8.97223C6.07082 5.12157 9.1924 2 13.0431 2C16.8937 2 20.0153 5.12157 20.0153 8.97223Z" fill="#4064BF"/>-->
+<!--          </svg>-->
+<!--        </a-button>-->
+<!--      </template>-->
+<!--    </a-input-search>-->
     <a-space class="mr-12">
       <!-- <a-button size="middle" type="primary">+ Новая сделка</a-button> -->
     </a-space>
     <DateTimeHeader class="mr-12"/>
-    <div class="mr-12 flex items-center"><IconDollar/> - <span class="usd">{{ currencyDataUSD }}</span></div>
-    <div class="mr-12 flex items-center"><IconEuro/> - <span class="eur">{{ currencyDataEURO }}</span></div>
+    <div class="mr-12 flex items-center"><IconDollar/> <span class="usd">{{ currencyDataUSD }}</span></div>
+    <div class="mr-12 flex items-center"><IconEuro/> <span class="eur">{{ currencyDataEURO }}</span></div>
     <NotificationHeader class="mr-12"/>
     <UserHeader :userData="getMyData" />
   </header>
@@ -61,7 +61,7 @@ const getMyData = computed(() => {
 
 onMounted(() => {
   fetchMe();
-  // fetchCurrencyData()
+  fetchCurrencyData()
 })
 
 const fetchMe = async () => {
@@ -89,9 +89,8 @@ const fetchCurrencyData = async () => {
           if (response.data.result === 'error') {
             message.error(response.data.text)
           } else {
-            console.log("response.data", response.data)
-            currencyDataEURO.value = response.data.EUR;
-            currencyDataUSD.value = response.data.USD;
+            currencyDataEURO.value = response.data.data.EUR;
+            currencyDataUSD.value = response.data.data.USD;
           }
         }
     )
