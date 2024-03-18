106fef2fe0e2f44c65a49dd4faf8e787e878b039
Ildus <solotrek93@gmail.com>
2024-03-18 15:53:39 +0300

    fix style textarea

--- src/components/objects/tabs/edit.vue
+++ src/components/objects/tabs/edit.vue
@@ -9,7 +9,7 @@
         <a-card class="mb-m-base/2" v-for="card in objectFields" :key="card.title">
           <div class="font-bold">{{ card.title }}</div>
           <a-divider/>
-          <div class="flex gap-[1.6rem] flex-wrap">
+          <div :class="(card.id == 240)?'block-full-width':'flex gap-[1.6rem] flex-wrap'">
             <div v-for="row in card.fields" :key="row.id">
               <a-form-item
                   v-if="row.type === 'text' || row.type === 'number'"
